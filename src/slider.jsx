aabc7ed43a96f8f4baa7ca948ae341e3ef09b77e
Your Name <emmanuelosawe90@gmail.com>
2025-12-14 14:39:42 -0800

    what are you looking for

--- src/slider.jsx
+++ src/slider.jsx
@@ -1,7 +1,18 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Autoplay } from "swiper/modules";
+import { useRef } from "react";
 
 const Hero = () => {
+  const swiperRef = useRef(null);
+
+  const stopAutoplay = () => {
+    swiperRef.current?.autoplay.stop();
+  };
+
+  const startAutoplay = () => {
+    swiperRef.current?.autoplay.start();
+  };
+
   return (
     <section className="hero-area">
       <Swiper
@@ -13,15 +24,16 @@ const Hero = () => {
           disableOnInteraction: false,
         }}
         navigation={true}
+        onSwiper={(swiper) => (swiperRef.current = swiper)}
         className="hero-slider-one"
       >
+
         {/* SLIDE 1 */}
         <SwiperSlide>
           <div
             className="single-hero-slider bg_cover"
             style={{
-              backgroundImage:
-                "url(assets/images/bg/hero-bg-slider-1.jpg)",
+              backgroundImage: "url(assets/images/bg/hero-bg-slider-1.jpg)",
             }}
           >
             <div className="container">
@@ -29,10 +41,6 @@ const Hero = () => {
                 <div className="col-lg-11">
                   <div className="hero-content text-center">
                     <h1>Build Your Dream With Our Construction Equipment</h1>
-                    <p>
-                      Aenean ligula porttitor euonsequat vitae eleifend aenliquam
-                      lorem ante dapibus in viverra quis feugiat a tellus.
-                    </p>
 
                     <div className="hero-search-wrapper">
                       <form>
@@ -42,23 +50,39 @@ const Hero = () => {
                               type="text"
                               className="form_control"
                               placeholder="What are you looking for?"
+                              onFocus={stopAutoplay}
+                              onBlur={startAutoplay}
                             />
                           </div>
+
                           <div className="col-lg-3">
-                            <select className="wide">
+                            <select
+                              className="form_control wide"
+                              onFocus={stopAutoplay}
+                              onBlur={startAutoplay}
+                            >
                               <option>Equipments</option>
                               <option>Machines</option>
                               <option>Tools</option>
                             </select>
                           </div>
+
                           <div className="col-lg-4">
                             <input
                               type="date"
                               className="form_control"
+                              onFocus={stopAutoplay}
+                              onBlur={startAutoplay}
                             />
                           </div>
+
                           <div className="col-lg-2">
-                            <button className="search-btn">
+                            <button
+                              className="search-btn"
+                              type="button"
+                              onMouseEnter={stopAutoplay}
+                              onMouseLeave={startAutoplay}
+                            >
                               Search
                             </button>
                           </div>
@@ -78,21 +102,64 @@ const Hero = () => {
           <div
             className="single-hero-slider bg_cover"
             style={{
-              backgroundImage:
-                "url(assets/images/bg/hero-bg-slider-2.jpg)",
+              backgroundImage: "url(assets/images/bg/hero-bg-slider-2.jpg)",
             }}
           >
             <div className="container">
               <div className="row justify-content-center">
                 <div className="col-lg-11">
                   <div className="hero-content text-center">
-                    <h1>
-                      We Provide Best Construction Equipment For You
-                    </h1>
-                    <p>
-                      Aenean ligula porttitor euonsequat vitae eleifend aenliquam
-                      lorem ante dapibus in viverra quis feugiat a tellus.
-                    </p>
+                    <h1>We Provide Best Construction Equipment For You</h1>
+
+                    {/* SAME FORM (REUSED LOGIC) */}
+                    <div className="hero-search-wrapper">
+                      <form>
+                        <div className="row">
+                          <div className="col-lg-3">
+                            <input
+                              type="text"
+                              className="form_control"
+                              placeholder="What are you looking for?"
+                              onFocus={stopAutoplay}
+                              onBlur={startAutoplay}
+                            />
+                          </div>
+
+                          <div className="col-lg-3">
+                            <select
+                              className="form_control wide"
+                              onFocus={stopAutoplay}
+                              onBlur={startAutoplay}
+                            >
+                              <option>Equipments</option>
+                              <option>Machines</option>
+                              <option>Tools</option>
+                            </select>
+                          </div>
+
+                          <div className="col-lg-4">
+                            <input
+                              type="date"
+                              className="form_control"
+                              onFocus={stopAutoplay}
+                              onBlur={startAutoplay}
+                            />
+                          </div>
+
+                          <div className="col-lg-2">
+                            <button
+                              className="search-btn"
+                              type="button"
+                              onMouseEnter={stopAutoplay}
+                              onMouseLeave={startAutoplay}
+                            >
+                              Search
+                            </button>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+
                   </div>
                 </div>
               </div>
