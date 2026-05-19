0939b73e5cdf003b9f5857445d6579657b547673
Your Name <emmanuelosawe90@gmail.com>
2026-05-18 23:01:54 -0700

    update

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Equip from "./equipments"; 
 import Contact from "./contact";
+import Admin from "./Admin";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/equipments" element={<Equip />} />
+      <Route path="/admin" element={<Admin />} />
       <Route path="contact" element={<Contact />} />
     </Routes>
   );
